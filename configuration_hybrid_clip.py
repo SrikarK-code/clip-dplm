3b7c793109b42e16f0b9f0aee98fc300dd623d35
Srikar Kavirayuni <84162099+SrikarK-code@users.noreply.github.com>
2024-10-05 10:29:04 -0700

    Update configuration_hybrid_clip.py

--- configuration_hybrid_clip.py
+++ configuration_hybrid_clip.py
@@ -1,5 +1,5 @@
 import copy
-from transformers.configuration_utils import PretrainedConfig
+from transformers import PretrainedConfig
 from transformers.utils import logging
 
 logger = logging.get_logger(__name__)
